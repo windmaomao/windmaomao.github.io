993acbe6e4cb854b108495ecd14a605b36de6bd1
Fang Jin <windmaomao@gmail.com>
2018-02-10 09:55:45 -0500

    exp with toc

--- app2/src/components/Main.vue
+++ app2/src/components/Main.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="main" v-bind:class="{ 'with-sidebar': sidebar }">
-    <template v-if="source">
-      <div class="sidebar" v-show="sidebar">
-        <div id="toc"></div>
-        <div id="back-top">
-            <a>Back to top</a>
-        </div>
+    <div class="sidebar" v-show="sidebar">
+      <div id="toc" data-toc="h2,h3"></div>
+      <div id="back-top">
+          <a>Back to top</a>
       </div>
-      <section class="section">
-        <div id="write" class="container" v-html="compiled"></div>
-      </section>
-    </template>
+    </div>
+    <section class="section">
+      <div id="write" class="container" v-html="compiled"></div>
+    </section>
   </div>
 </template>
 
 <script>
 var md = require('markdown-it')()
+// var toc = require('../../static/js/toc.js')
 
 export default {
   name: 'Main',
@@ -29,6 +28,17 @@ export default {
     compiled: function() {
       return md.render(this.source)
     }
+  },
+  watch: {
+    compiled: function() {
+      console.log('compiled')
+    }
+  },
+  created: function() {
+    // console.log(document.getElementById("toc"))
+  },
+  mounted: function() {
+    console.log(document.getElementById('toc'))
   }
 }
 </script>
@@ -39,5 +49,16 @@ export default {
   margin: 0 auto;
   padding-top: 2em 60px;
   min-height: calc(100vh - 6em);
+}
+.sidebar {
+  position: fixed;
+  right: 0;
+  top: 100px;
+  bottom: 5px;
+  width: 240px;
+  margin: 0;
+  font-size: 12px;
+  border-left: 1px solid #eee;
+  overflow-y: auto;
 }
 </style>
