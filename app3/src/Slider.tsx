07602930ee9d1828b4722c6e1a637555fcf687b5
Fang Jin <windmaomao@gmail.com>
2018-02-19 16:08:58 -0500

    add style to sidebar

--- app3/src/Slider.tsx
+++ app3/src/Slider.tsx
@@ -6,9 +6,30 @@ export interface Props {
 }
 
 export default function Slider(props: Props) {
+  let menus = [
+    { title: 'Books', items: ['index'] },
+    { title: 'Blogs', items: ['index'] },
+    { title: 'Todo', items: ['fang', 'china-trip'] }
+  ];
   return (
     <div className="slider">
-      <h1>Slider</h1>
+      <aside className="menu">
+        <p className="menu-label">Menu</p>
+        <ul className="menu-list">{
+          menus.map((menu, idx) =>
+          <li key={idx}>
+            <a>{menu.title}</a>
+            <ul>{
+              menu.items.map((name, idx2) =>
+                <li key={idx2}>
+                  <a>{name}</a>
+                </li>
+              )
+            }</ul>
+          </li>
+          )
+        }</ul>
+      </aside>
     </div>
   );
 }
