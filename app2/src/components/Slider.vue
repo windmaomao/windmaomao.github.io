263bb4b918717ae18a1e54733136e787d48175d7
Fang Jin <windmaomao@gmail.com>
2018-02-21 17:56:19 -0500

    add slider toggle

--- app2/src/components/Slider.vue
+++ app2/src/components/Slider.vue
@@ -1,31 +1,49 @@
 <script>
 export default {
   name: 'Slider',
+  data: () => ({
+    enabled: true,
+    open: false
+  }),
   render() {
     let menus = [
       { title: 'Blogs', items: [] },
       { title: 'Books', items: ['index', 'vue-up'] }
     ]
-    return (
-      <div class="slider">
-        <button class="close">X</button>
-        <aside className="menu">
-          <p className="menu-label">Menu</p>
-          <ul className="menu-list">{
-            menus.map((menu) =>
-              <li>
-                <a>{menu.title}</a>
-                <ul>{
-                  menu.items.map((name) =>
-                    <li><a>{name}</a></li>
-                  )
-                }</ul>
-              </li>
-            )
-          }</ul>
-        </aside>
-      </div>
-    )
+    if (!this.enabled) {
+      return null
+    }
+    if (this.open) {
+      return (
+        <div class="slider">
+          <button class="close" onClick={this.onToggle}>X</button>
+          <aside className="menu">
+            <p className="menu-label">Menu</p>
+            <ul className="menu-list">{
+              menus.map((menu) =>
+                <li>
+                  <a>{menu.title}</a>
+                  <ul>{
+                    menu.items.map((name) =>
+                      <li><a>{name}</a></li>
+                    )
+                  }</ul>
+                </li>
+              )
+            }</ul>
+          </aside>
+        </div>
+      )
+    } else {
+      return (
+        <i class="slider-toggle" onClick={this.onToggle} />
+      )
+    }
+  },
+  methods: {
+    onToggle(e) {
+      this.open = !this.open
+    }
   }
 }
 </script>
