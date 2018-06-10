3e82ad0d1e5d2bef7fc572721f010599a1490b05
Fang Jin <windmaomao@gmail.com>
2018-06-10 14:33:24 -0400

    left over

--- app2/src/components/Stocker.vue
+++ app2/src/components/Stocker.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="stocker">
     <div class="control autocomplete">
-      <input class="input is-small" type="text" placeholder="Stock Symbol">
-      <ul class="autocomplete-results">
+      <input class="input is-small"
+        type="text" placeholder="Stock Symbol"
+        v-model="search" @input="onChange"
+      />
+      <ul class="autocomplete-results" v-show="active">
         <li>
           <span>ADSK</span>: +3.4%
         </li>
@@ -16,8 +19,15 @@ export default {
   name: 'Stocker',
   props: {},
   data: () => ({
+    active: false,
+    search: ''
   }),
   created() {
+  },
+  methods: {
+    onChange() {
+      this.active = true
+    }
   }
 }
 </script>
