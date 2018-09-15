a7a9ff6e3b7c583fa7fd9de5b50dded1af2ae306
Fang Jin <windmaomao@gmail.com>
2018-09-15 16:29:40 -0400

    assemble teacher list id

--- source/scheduler/src/App.js
+++ source/scheduler/src/App.js
@@ -2,15 +2,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {MuiThemeProvider, withStyles} from '@material-ui/core/styles';
+import {map} from 'lodash';
 // styles
 import './App.css';
+import theme from './theme';
 // primary components
 import CssBaseline from '@material-ui/core/CssBaseline';
 // secondary components
 import Navbar from './Navbar';
-import Main from './Main';
+import Plan from './Plan';
 // services
-import theme from './theme';
+import SchedulerService from './Scheduler';
+import {teachersInfo, studentsInfo, prefsInfo} from './data';
 
 const styles = theme => ({
   layout: {
@@ -26,14 +29,25 @@ const styles = theme => ({
 });
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ss = new SchedulerService();
+    this.state = {
+      slots: this.ss.fillSlots(studentsInfo, teachersInfo, prefsInfo),
+      ids: map(teachersInfo, 'id'),
+      errors: this.ss.errors
+    }
+  }
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
+    const {slots, ids, errors} = this.state;
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <Navbar />
         <div className={classes.layout}>
-          <Main />
+          <h1>Slots Available</h1>
+          <Plan slots={slots} ids={ids} />
         </div>
       </MuiThemeProvider>
     );
