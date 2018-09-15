b046e5f17a4fe4474875a39f80aacd75e57ad8ad
Fang Jin <windmaomao@gmail.com>
2018-09-15 14:52:47 -0400

    add material theme

--- source/scheduler/src/App.js
+++ source/scheduler/src/App.js
@@ -1,19 +1,22 @@
 // third party
 import React, { Component } from 'react';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 // styles
 import './App.css';
 // primary components
 // secondary components
 import Navbar from './Navbar';
 import Main from './Main';
+// services
+import theme from './theme';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme}>
         <Navbar />
         <Main />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ source/scheduler/src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+  },
+});
+
+export default theme;
