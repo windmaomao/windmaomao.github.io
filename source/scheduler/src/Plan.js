975f53a1b26483b9885d8349866353f0770a0826
Fang Jin <windmaomao@gmail.com>
2018-09-16 11:03:01 -0400

    use list instead of table

--- source/scheduler/src/Plan.js
+++ source/scheduler/src/Plan.js
@@ -6,16 +6,19 @@ import {withStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
 
 const styles = { 
 	narrowCell: {
@@ -78,13 +81,65 @@ class Plan extends Component {
     )
   }
 
+  list(arr) {
+    if (!arr) return '  ';
+    return (
+      <div style={{display: 'flex'}}>
+        {arr.map(item => (
+          <Chip style={{marginRight: '0.2rem', padding: '0'}}
+            key={item} 
+            label={item}
+            color="primary" 
+            variant="outlined" 
+          />
+        ))}
+      </div>
+    );
+  }
+
+  schedule(id) {
+    const {slots} = this.props;
+    return (
+      <List component="nav">
+        {Object.keys(slots).map(slot => (
+          <ListItem key={slot} button>
+            <ListItemIcon>
+              <span>{this.slot2time(slot)}</span>
+            </ListItemIcon>
+            <ListItemText inset primary={this.list(slots[slot][id])} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
+
+  card(id) {
+    const {usages} = this.props;
+    return (
+      <Card>
+        <CardHeader 
+          avatar={<Avatar>{id[0]}</Avatar>}
+          title={id + ' (' + usages[id] + ')'}
+          subheader={'July 16, 3:00 - 7:00'}
+        />
+        <CardContent>
+          {this.schedule(id)}
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    )
+  }
+
   render() {
     const {ids} = this.props;
     return (
       <Grid container spacing={24}>
         {ids.map(id => (
           <Grid key={id} item xs={12} sm={6} md={4}>
-            {this.slots([id])}
+            {/* {this.slots([id])} */}
+            {this.card(id)}
           </Grid>          
         ))}
       </Grid>
