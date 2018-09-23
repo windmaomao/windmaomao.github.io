f979383e351dbcbf87889a88cc189b8a330720fe
Fang Jin <windmaomao@gmail.com>
2018-09-23 16:51:35 -0400

    relocate functions to schedule page

--- source/scheduler/src/App.js
+++ source/scheduler/src/App.js
@@ -1,156 +1,33 @@
 // third party
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {MuiThemeProvider, withStyles} from '@material-ui/core/styles';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 // styles
 import './App.css';
 import theme from './theme';
 // primary components
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
 // secondary components
 import Navbar from './Navbar';
-import Plan from './Plan';
-import Print from './Print';
-import Upload from './Upload';
 import Schedule from './Schedule';
 import Student from './Student';
 // services
-import SchedulerService from './Scheduler';
-import {teachersInfo, studentsInfo, prefsInfo} from './data1';
-import ApiService from './Api';
-
-const styles = theme => ({
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1600 + theme.spacing.unit * 3 * 2)]: {
-      width: 1600,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-});
 
 class App extends Component {
-  // debug = true;
-  debug = false;
-
-  constructor(props) {
-    super(props);
-    this.ss = new SchedulerService();
-
-    this.data = {
-      students: studentsInfo,
-      teachers: teachersInfo,
-      preferences: prefsInfo
-    };
-
-    this.state = {
-      loading: true,
-      slots: {},
-      usages: {},
-      ids: [],
-      total: 0,
-      errors: [],
-    };
-  }
-
-  componentDidMount() {
-    this.initPlan();
-  }
-
-  updatePlan() {
-    const {teachers, students, preferences} = this.info;
-    this.setState({
-      loading: false,
-      slots: this.ss.fillSlots(students, teachers, preferences),
-      usages: this.ss.teacherUsage,
-      ids: Object.keys(this.ss.teacherUsage),
-      total: this.ss.totalSlots(),
-      errors: this.ss.errors
-    });
-  }
-
-  initPlan() {
-    if (this.debug) {
-      this.updatePlan();
-    } else {
-      this.setState({loading: true});
-      ApiService.getInfo().then(data => {
-        const {teachers, students, preferences} = data;
-        this.info = {teachers, students, preferences};
-        this.updatePlan();
-      });
-    }
-  }
-
-  title(total) {
-    const {loading} = this.props;
-    return (
-      <div className={'App-title'}>
-        <Button 
-          variant="contained" color="secondary" style={{float: 'right'}}
-          onClick={() => {this.updatePlan();}}
-          disabled={loading}
-        >Plan</Button>
-        <h1>Today <small>({total})</small></h1>
-      </div>
-    );
-  }
-
-  error(errors) {
-    return <p style={{ color: 'red' }}>{errors.join(',')}</p>;
-  }
-
-  main() {
-    const {classes} = this.props;
-    const {loading, slots, ids, usages, total, errors} = this.state;
-    return (
-      <div className={classes.layout}>
-        <Upload onDataUpload={this.onUpload} />
-        {this.title(total)}
-        {this.error(errors)}
-        {!loading && <Plan slots={slots} ids={ids} usages={usages} />}
-      </div>
-    );
-  }
-
-  print() {
-    const {loading, slots, usages} = this.state;
-    return (
-      <div>
-        {!loading && <Print slots={slots} usages={usages} />}
-      </div>
-    )
-  }
-
-  onUpload = (data) => {
-    this.info.students = data;
-    this.updatePlan();
-  }
-
   render() {
     return (
       <Router>
         <MuiThemeProvider theme={theme}>
           <CssBaseline />
           <Navbar />
+          <Link to="/">Home</Link>
           <Link to="/student">Student</Link>
           <Route exact path="/" component={Schedule} />
           <Route path="/student" component={Student} />
-          {this.print()}
-          {this.main()}
         </MuiThemeProvider>
       </Router>
     );
   }
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(App);
+export default App;
--- source/scheduler/src/Schedule.js
+++ source/scheduler/src/Schedule.js
@@ -1,16 +1,142 @@
 // third party
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
 // styles
 // primary components
+import Button from '@material-ui/core/Button';
 // secondary components
+import Plan from './Plan';
+import Print from './Print';
+import Upload from './Upload';
+// services
+import SchedulerService from './Scheduler';
+import {teachersInfo, studentsInfo, prefsInfo} from './data1';
+import ApiService from './Api';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1600 + theme.spacing.unit * 3 * 2)]: {
+      width: 1600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+});
 
 class Schedule extends Component {
+  // debug = true;
+  debug = false;
+
+  constructor(props) {
+    super(props);
+    this.ss = new SchedulerService();
+
+    this.data = {
+      students: studentsInfo,
+      teachers: teachersInfo,
+      preferences: prefsInfo
+    };
+
+    this.state = {
+      loading: true,
+      slots: {},
+      usages: {},
+      ids: [],
+      total: 0,
+      errors: [],
+    };
+  }
+
+  componentDidMount() {
+    this.initPlan();
+  }
+
+  updatePlan() {
+    const {teachers, students, preferences} = this.info;
+    this.setState({
+      loading: false,
+      slots: this.ss.fillSlots(students, teachers, preferences),
+      usages: this.ss.teacherUsage,
+      ids: Object.keys(this.ss.teacherUsage),
+      total: this.ss.totalSlots(),
+      errors: this.ss.errors
+    });
+  }
+
+  initPlan() {
+    if (this.debug) {
+      this.updatePlan();
+    } else {
+      this.setState({loading: true});
+      ApiService.getInfo().then(data => {
+        const {teachers, students, preferences} = data;
+        this.info = {teachers, students, preferences};
+        this.updatePlan();
+      });
+    }
+  }
+
+  title(total) {
+    const {loading} = this.props;
+    return (
+      <div className={'App-title'}>
+        <Button 
+          variant="contained" color="secondary" style={{float: 'right'}}
+          onClick={() => {this.updatePlan();}}
+          disabled={loading}
+        >Plan</Button>
+        <h1>Today <small>({total})</small></h1>
+      </div>
+    );
+  }
+
+  error(errors) {
+    return <p style={{ color: 'red' }}>{errors.join(',')}</p>;
+  }
+
+  main() {
+    const {classes} = this.props;
+    const {loading, slots, ids, usages, total, errors} = this.state;
+    return (
+      <div className={classes.layout}>
+        <Upload onDataUpload={this.onUpload} />
+        {this.title(total)}
+        {this.error(errors)}
+        {!loading && <Plan slots={slots} ids={ids} usages={usages} />}
+      </div>
+    );
+  }
+
+  print() {
+    const {loading, slots, usages} = this.state;
+    return (
+      <div>
+        {!loading && <Print slots={slots} usages={usages} />}
+      </div>
+    )
+  }
+
+  onUpload = (data) => {
+    this.info.students = data;
+    this.updatePlan();
+  }
+
   render() {
     return (
-        <h1>Schedule Page</h1>
+      <div>
+        {this.print()}
+        {this.main()}
+      </div>
     );
   }
 }
-  
-export default Schedule;
-  
+
+Schedule.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Schedule);
