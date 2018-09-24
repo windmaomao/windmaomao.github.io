8080fa31d46b8ec59949670209363d67aba9349d
Fang Jin <windmaomao@gmail.com>
2018-09-24 14:56:42 -0400

    add markdown rendering

--- source/kb2/src/Article.js
+++ source/kb2/src/Article.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 class Article extends Component {
   render() {
+    const input = '# This is a header\n\nAnd this is a paragraph'
     return (
+      <div class="content">
         <h1>Article</h1>
+        <ReactMarkdown source={input} />
+      </div>
     )
   }
 }
