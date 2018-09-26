d4cb93e0e26c1fa8f1c709f82c8ca4e9ad9c2c8a
Fang Jin <windmaomao@gmail.com>
2018-09-25 21:52:48 -0400

    add anchor parser to markdown service

--- source/kb2/src/MarkdownService.js
+++ source/kb2/src/MarkdownService.js
@@ -1,20 +1,36 @@
 // Markdown core and highlighter
-const md = require('markdown-it')({
-  html: true,
-  linkify: true,
-});
-md.use(require('markdown-it-attrs/markdown-it-attrs.browser.js'));
-md.use(require('markdown-it-emoji'));
 
 export default class MarkdownService {
+  md = null;
+  anchors = [];
+  constructor() {
+    this.md = require('markdown-it')({
+      html: true,
+      linkify: true,
+    });
+    this.md.use(require('markdown-it-attrs/markdown-it-attrs.browser.js'));
+    this.md.use(require('markdown-it-emoji'));
+    this.md.use(require('markdown-it-anchor'), { callback: this.parseHeading });
+  }
+
+  parseHeading = (token, values) => {
+    const lvl = {
+      tag: token.tag,
+      title: values.title,
+      anchor: values.slug
+    };
+    this.anchors.push(lvl);
+  }
+
   render(source) {
-    return md.render(source);
+    this.anchors = [];
+    const html = this.md.render(source);
+    return html;
   }
 }
 
 const mdService = new MarkdownService();
   
 export {
-  md,
   mdService
 };
