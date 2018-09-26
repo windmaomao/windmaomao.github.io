c9f23f22b6dc021353c8c76514bed80a99209cd3
Fang Jin <windmaomao@gmail.com>
2018-09-26 16:11:03 -0400

    add get apps api

--- source/kb2/src/Api.js
+++ source/kb2/src/Api.js
@@ -1,6 +1,11 @@
+const DOMAIN = 'https://sleepy-kalam-ff10a0.netlify.com/';
+const APP = 'kb/';
+const DATA = 'static/';
+
 export default class ApiService {
   // static domain = 'http://localhost:3000/';
-  static domain = 'https://sleepy-kalam-ff10a0.netlify.com/kb/';
+  static domain = DOMAIN + APP;
+  static data = DOMAIN + DATA;
   
   static getArticle(id) {
     const url = ApiService.domain + id + '.md?v=' + Date.now();
@@ -9,6 +14,11 @@ export default class ApiService {
 
   static getMenu() {
     const url = this.domain + 'menu.json';
+    return fetch(url).then(res => res.json());
+  }
+
+  static getApps() {
+    const url = this.domain + 'app.json';
     return fetch(url).then(res => res.json());
   }
 }
