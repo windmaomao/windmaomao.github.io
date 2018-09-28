7921dcfcca515825f23e9bb0f032bed5a22adcfe
Fang Jin <windmaomao@gmail.com>
2018-09-28 16:11:54 -0400

    simplify article with pure component

--- source/kb2/src/Article.js
+++ source/kb2/src/Article.js
@@ -1,52 +1,41 @@
 // third party
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {toJS} from 'mobx';
 // styles
 import './Article.css';
 // primary component
 import Toc from './Toc';
 
-class Article extends Component {
-  static propTypes = {
-    article: PropTypes.object
-  };
+const Breadcrumb = (id) => {
+  const bcs = id.split('/');
+  return (
+    <nav className="breadcrumb is-small" aria-label="breadcrumbs">
+      <ul>
+        {bcs.map((item, index) => (
+          <li key={index} className="is-active">
+            <a>{item.toUpperCase()}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>      
+  );
+};
 
-  renderBreadcrumb() {
-    const {article} = this.props;
-    const bcs = article.id.split('/');
-    return (
-      <nav className="breadcrumb is-small" aria-label="breadcrumbs">
-        <ul>
-          {bcs.map((item, index) => (
-            <li key={index} className="is-active">
-              <a>{item.toUpperCase()}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>      
-    );
-  }
-
-  renderToc() {
-    const {article} = this.props;
-    return (
+const Article = (props) => {
+  const {article} = props;
+  return (
+    <div className="">
       <Toc anchors={article.anchors} />
-    );
-  }
-
-  render() {
-    const {article} = this.props; console.log(toJS(article));
-    return (
-      <div className="">
-        {/* {this.renderToc()} */}
-        <div className={'section container article'}>
-          {this.renderBreadcrumb()}
-          <div dangerouslySetInnerHTML={{__html: article.html}} />
-        </div>
+      <div className={'section container article'}>
+        {Breadcrumb(article.id)}
+        <div dangerouslySetInnerHTML={{__html: article.html}} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Article.propTypes = {
+  article: PropTypes.object
+};
 
 export default Article;
