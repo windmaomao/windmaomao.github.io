6a85bbab9d9d894e17a4a33d95bb3c26de4c1a49
Fang Jin <windmaomao@gmail.com>
2018-09-28 17:00:04 -0400

    convert navbar into pure component

--- source/kb2/src/Navbar.js
+++ source/kb2/src/Navbar.js
@@ -1,57 +1,39 @@
 // third party
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    onSidenavToggle: PropTypes.func,
-    onAppnavClick: PropTypes.func,
+const Navbar = (props) => {
+  const {title} = props;
+  const toggle = () => {
+    const onChange = props.onSidenavToggle;
+    onChange && onChange();
+  };
+  const trigger = () => {
+    const onTrigger = props.onAppnavClick;
+    onTrigger && onTrigger();
   };
 
-  toggle = () => {
-    const onChange = this.props.onSidenavToggle;
-    onChange && onChange();
-  }
-
-  trigger = () => {
-    const onTrigger = this.props.onAppnavClick;
-    onTrigger && onTrigger();
-  }
-
-  renderToc() {
-    return (
-      <div className="navbar-menu is-active">
-        <div className="navbar-end">
-          <div className="navbar-item has-dropdown is-active">
-            <a className="navbar-link">Table of Content</a>
-            <div className="navbar-dropdwon">
-            </div>
-          </div>
+  return (
+    <nav className="navbar is-dark is-fixed-top">
+      <div className="navbar-brand">
+        <a className="navbar-item logo-wrapper" onClick={e => trigger()}>
+          <img src="logo.png" className="logo" alt="logo" />
+        </a>
+        <a className="navbar-item" onClick={e => toggle()}>{title}</a>
+        <div data-target="navbar">
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
-    );
-  }
+    </nav>
+  );
+};
 
-  render() {
-    const {title} = this.props;
-    return (
-      <nav className="navbar is-dark is-fixed-top">
-        <div className="navbar-brand">
-          <a className="navbar-item logo-wrapper" onClick={e => this.trigger()}>
-            <img src="logo.png" className="logo" alt="logo" />
-          </a>
-          <a className="navbar-item" onClick={e => this.toggle()}>{title}</a>
-          <div data-target="navbar">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        {/* {this.renderToc()} */}
-      </nav>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string,
+  onSidenavToggle: PropTypes.func,
+  onAppnavClick: PropTypes.func,
+};
 
 export default Navbar;
