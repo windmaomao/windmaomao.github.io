1937dfe04b7fb59d4b00a435dad17e7673058b68
Fang Jin <windmaomao@gmail.com>
2018-10-04 11:04:11 -0400

    add table semantic ui

--- source/scheduler/src/Print2.js
+++ source/scheduler/src/Print2.js
@@ -1,11 +1,77 @@
 // third party
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactToPrint from "react-to-print";
+// components
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
 class Print extends Component {
+
+  slots() {
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Header</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <Label ribbon>First</Label>
+            </Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan='3'>
+              <Menu floated='right' pagination>
+                <Menu.Item as='a' icon>
+                  <Icon name='chevron left' />
+                </Menu.Item>
+                <Menu.Item as='a'>1</Menu.Item>
+                <Menu.Item as='a'>2</Menu.Item>
+                <Menu.Item as='a'>3</Menu.Item>
+                <Menu.Item as='a'>4</Menu.Item>
+                <Menu.Item as='a' icon>
+                  <Icon name='chevron right' />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>             
+    );
+  }
+
   render() {
     return (
-      <h1>Today Schedule</h1>
+      <div>
+        <ReactToPrint trigger={() => <a>Print</a>}
+          content={() => this.componentRef}
+        />
+        <div ref={el => (this.componentRef = el)} className="allow-print">
+          <h1>Today's Schedule</h1>
+          {this.slots()}
+        </div>
+ 
+      </div>
     )
   }
 };
