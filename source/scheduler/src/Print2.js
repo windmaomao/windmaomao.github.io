33f6fe85895f6da7ec9c45d551ca6973562ce3b9
Fang Jin <windmaomao@gmail.com>
2018-10-04 11:12:02 -0400

    make sure table supports colspan

--- source/scheduler/src/Print2.js
+++ source/scheduler/src/Print2.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactToPrint from "react-to-print";
 // components
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Label, Table } from 'semantic-ui-react'
 
 class Print extends Component {
 
@@ -12,9 +12,18 @@ class Print extends Component {
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Slot</Table.HeaderCell>
+            <Table.HeaderCell colSpan="3">T1</Table.HeaderCell>
+            <Table.HeaderCell colSpan="3">T2</Table.HeaderCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.HeaderCell></Table.HeaderCell>
+            <Table.HeaderCell>S1</Table.HeaderCell>
+            <Table.HeaderCell>S2</Table.HeaderCell>
+            <Table.HeaderCell>S3</Table.HeaderCell>
+            <Table.HeaderCell>S1</Table.HeaderCell>
+            <Table.HeaderCell>S2</Table.HeaderCell>
+            <Table.HeaderCell>S3</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -25,37 +34,30 @@ class Print extends Component {
             </Table.Cell>
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
           </Table.Row>
           <Table.Row>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
+            <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
             <Table.Cell>Cell</Table.Cell>
           </Table.Row>
         </Table.Body>
-
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan='3'>
-              <Menu floated='right' pagination>
-                <Menu.Item as='a' icon>
-                  <Icon name='chevron left' />
-                </Menu.Item>
-                <Menu.Item as='a'>1</Menu.Item>
-                <Menu.Item as='a'>2</Menu.Item>
-                <Menu.Item as='a'>3</Menu.Item>
-                <Menu.Item as='a'>4</Menu.Item>
-                <Menu.Item as='a' icon>
-                  <Icon name='chevron right' />
-                </Menu.Item>
-              </Menu>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
       </Table>             
     );
   }
