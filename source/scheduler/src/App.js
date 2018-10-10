57e147e57205efc753d127805a0f252a9e001b6a
Fang Jin <windmaomao@gmail.com>
2018-10-09 21:48:55 -0400

    deploy scheduler with no dead lock

--- source/scheduler/src/App.js
+++ source/scheduler/src/App.js
@@ -11,35 +11,26 @@ import Pref from './Pref';
 // services
 import {scheduler} from './Scheduler';
 import {teachersInfo, studentsInfo, prefsInfo} from './data1';
-import ApiService from './Api';
+// import ApiService from './Api';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: true};
-  } 
 
   componentDidMount() {
-    const debug = true;
-    // const debug = false;
+    // const debug = true;
+    const debug = false;
     if (debug) {
       scheduler.setData(teachersInfo, studentsInfo, prefsInfo);
-      this.setState({loading: false});
-    } else {
-      ApiService.getInfo().then(data => {
-        const {teachers, students, preferences} = data;
-        scheduler.setData(teachers, students, preferences);
-        this.setState({loading: false});
-      });
     }
+    //  else {
+    //   ApiService.getInfo().then(data => {
+    //     const {teachers, students, preferences} = data;
+    //     scheduler.setData(teachers, students, preferences);
+    //     this.setState({loading: false});
+    //   });
+    // }
   }
 
   render() {
-    const {loading} = this.state;
-    if (loading) {
-      return null;
-    }
-
     return (
       <Fragment>
         <Route exact path="/" component={Schedule} />
