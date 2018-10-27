b3afea7c5f33216b411fa5bc7a697b60d7d66ef6
Fang Jin <windmaomao@gmail.com>
2018-10-27 16:57:42 -0400

    allow adding more cards

--- source/invest/src/App.js
+++ source/invest/src/App.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {observer} from 'mobx-react';
+// components
+import { Button } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import { Card } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
-    const {trades} = this.props.store;
+    const {store} = this.props;
     return (
-      <div className="App">
-      {trades.map(trade => (
-        <Card key={trade.name}>
-          <Card.Content>
-            <Card.Header>{trade.title}</Card.Header>
-            <Card.Meta>{trade.name}</Card.Meta>
-            <Card.Description>{trade.url}</Card.Description>
-          </Card.Content>
-        </Card>
-      ))}
-      </div>
+      <Container className="App">
+        <h1>List</h1>
+        {store.trades.map((trade, index) => (
+          <Card fluid key={index}>
+            <Card.Content>
+              <Card.Header>{trade.title}</Card.Header>
+              <Card.Meta>{trade.name}</Card.Meta>
+              <Card.Description>{trade.url}</Card.Description>
+            </Card.Content>
+          </Card>
+        ))}
+        <div>
+          <Button onClick={() => {store.addTrade();}}>Add Trade</Button>
+        </div>
+      </Container>
     );
   }
 }
@@ -25,4 +33,4 @@ App.propTypes = {
   store: PropTypes.object.isRequired
 }
 
-export default App;
+export default observer(App);
--- source/invest/src/store.js
+++ source/invest/src/store.js
@@ -1,12 +1,16 @@
-import {observable, decorate} from 'mobx';
+import {observable, decorate, action} from 'mobx';
 import { Trade } from './model';
 
 class Store {
   trades = [Trade];
+  addTrade() {
+    this.trades.push(Trade);
+  }
 }
 
 decorate(Store, {
-  trades: observable
+  trades: observable,
+  addTrade: action.bound,
 });
 
 export default Store;
