157fa97a8c5bf0d6c0a076ff952b401246ada007
Fang Jin <windmaomao@gmail.com>
2018-11-03 21:43:54 -0400

    use table to model summary list

--- source/invest/src/SummaryList/SummaryList.js
+++ source/invest/src/SummaryList/SummaryList.js
@@ -2,59 +2,52 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import NumberFormat from 'react-number-format';
 // components
-import { Image, List, Label, Icon, Button } from 'semantic-ui-react'
+import { Table, Image, List, Label, Icon, Button } from 'semantic-ui-react'
 
 class SummaryList extends Component {
   render() {
     const {items} = this.props;
     // const shares = <span>Shares: {summary.shares}</span>;
     return (
-      <List selection animated relaxed verticalAlign='top'>
-        {items.map(item => (
-          <List.Item>
-            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
-            <List.Content>
-              <List.Header>
-                {item.name}
-              </List.Header>
-              <List.Description>
-                <Label image size="mini" color="blue">
-                  Quantity
-                  <Label.Detail>
-                    <strong>{item.shares}</strong>
-                  </Label.Detail>
-                </Label>                
-                <Label image size="mini" color="green">
-                  Cost
-                  <Label.Detail>
-                    <strong>
-                      <NumberFormat value={item.cost} 
-                        displayType={'text'} 
-                        thousandSeparator={true} prefix={'$'} 
-                        decimalScale={0}
-                        fixedDecimalScale={true}
-                      />
-                    </strong>
-                  </Label.Detail>
-                </Label>     
-              </List.Description>
-            </List.Content>
-          </List.Item>
-        ))}
-      </List>      
-      // <Card fluid>
-      //   <Card.Content>
-      //     <Card.Header>
-      //       <Button basic size="mini" icon='world' floated="right" circular />
-      //       {summary.name}
-      //     </Card.Header>
-      //     <Card.Description>
-      //       <List>
-      //         <List.Item icon='chess' content={shares} />
-      //       </List>            
-      //     </Card.Description>
-      //   </Card.Content>
-      // </Card>
+      <Table 
+        textAlign="right" size="small" compact 
+        sortable unstackable 
+        basic='very' celled
+      >
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell>Cost</Table.HeaderCell>
+            <Table.HeaderCell>Action</Table.HeaderCell>
+          </Table.Row>
+          <Table.Row>
+            <Table.HeaderCell colSpan='4'>
+              
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {items.map(item => (
+            <Table.Row warning={item.quantity<0}>
+              <Table.Cell>{item.name}</Table.Cell>
+              <Table.Cell>{item.quantity}</Table.Cell>
+              <Table.Cell>
+                <NumberFormat value={item.cost} 
+                  displayType={'text'} 
+                  thousandSeparator={true} prefix={'$'} 
+                  decimalScale={0}
+                  fixedDecimalScale={true}
+                />
+              </Table.Cell>
+              <Table.Cell>
+                <Icon link name='close' />
+                <Icon link name='edit' />
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>           
     );
   }
 }
--- source/invest/src/SummaryList/summary.model.js
+++ source/invest/src/SummaryList/summary.model.js
@@ -7,17 +7,17 @@ const getDefaultSummary = () => ({
 });
 
 const summaries = [
-  { name: 'ADSK', shares: -13, cost: -1743.69 },
-  { name: 'ALNY', shares: -8, cost: -706.08 },
-  { name: 'BC', shares: -8, cost: -426.24 },
-  { name: 'BFAM', shares: -8, cost: -927.44 },
-  { name: 'BKS', shares: -80, cost: -536.00 },
-  { name: 'BLUE', shares: -6, cost: -796.14 },
-  { name: 'DTYS', shares: 130, cost: 3264.91 },
-  { name: 'EUM', shares: 130, cost: 2602.60 },
-  { name: 'CHAD', shares: 100, cost: 3706.00 },
-  { name: 'EFZ', shares: 25, cost: 692.75 },
-  { name: 'SH', shares: 40, cost: 1166.40 },
+  { name: 'ADSK', quantity: -13, cost: -1743.69 },
+  { name: 'ALNY', quantity: -8, cost: -706.08 },
+  { name: 'BC', quantity: -8, cost: -426.24 },
+  { name: 'BFAM', quantity: -8, cost: -927.44 },
+  { name: 'BKS', quantity: -80, cost: -536.00 },
+  { name: 'BLUE', quantity: -6, cost: -796.14 },
+  { name: 'DTYS', quantity: 130, cost: 3264.91 },
+  { name: 'EUM', quantity: 130, cost: 2602.60 },
+  { name: 'CHAD', quantity: 100, cost: 3706.00 },
+  { name: 'EFZ', quantity: 25, cost: 692.75 },
+  { name: 'SH', quantity: 40, cost: 1166.40 },
 ]
 
 export {
