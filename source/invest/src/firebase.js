797af9f3d286dac08ec689fa0fd655c0238be542
Fang Jin <windmaomao@gmail.com>
2018-11-06 18:06:46 -0500

    remove firebase config

--- source/invest/src/firebase.js
+++ source/invest/src/firebase.js
@@ -1,15 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/app';
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyD_6NUanoEgY8S_aA_FxJRyESrEzkUgUk4",
-  authDomain: "torrid-heat-2030.firebaseapp.com",
-  databaseURL: "https://torrid-heat-2030.firebaseio.com",
-  projectId: "torrid-heat-2030",
-  storageBucket: "torrid-heat-2030.appspot.com",
-  messagingSenderId: "829342570880"
-};
+import config from './.firebase';
 firebase.initializeApp(config);
 
 export default firebase;
