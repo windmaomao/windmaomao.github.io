51052b73ad65cd6d68c7b4bf700cfb9a2348e377
Fang Jin <windmaomao@gmail.com>
2018-12-03 11:27:12 -0500

    support tree rows structure

--- source/playground/src/Outliner/OutlinerStore.js
+++ source/playground/src/Outliner/OutlinerStore.js
@@ -1,24 +1,52 @@
 // libraries
-import {decorate, observable, action} from 'mobx';
+import {decorate, observable, action, toJS} from 'mobx';
 // services
 import {getXml} from '../utils/callApi';
 // locals
 const MarkdownIt = require('markdown-it'), 
   md = new MarkdownIt({ breaks: true });
 
+
+// Given a tree root, return row configs
+function setupTreeRows(root) {
+  const rows = [];
+  let idGen = 0;
+
+  const assignAttrs = (node, level) => {
+    // assign node id
+    node.id = idGen++;
+    const row = {
+      level: level,
+      collapsed: false,
+      node: node,
+    };
+    rows.push(row);
+    node.children.forEach(item => {
+      assignAttrs(item, level + 1);
+    });
+  }
+
+  assignAttrs(root, 0);
+  return rows;
+}
+
 class OutlinerStore {
   root = {};
+  rows = [];
   options = {
     outliner: this,
     markdown: md,
     noteInRow: true,
-    hiddenCols: []
+    hiddenCols: [],
+    searchText: 'ABC',
   };
 
   fetchOutliner() {
-    return getXml('opml/resume.opml').then(res => {
+    return getXml('opml/projects.opml').then(res => {
       this.root = res.opml.body;
-      console.log(this.root);
+      this.rows = setupTreeRows(this.root);
+      console.log(toJS(this.root));
+      console.log(toJS(this.options));
       return res;
     });
   }
@@ -32,13 +60,18 @@ class OutlinerStore {
       hiddens.splice(index, 1);
     }
   }
+
+  toggleRowCollapsed(row) {
+    row.collapsed = !row.collapsed;
+  }
 }
 
 decorate(OutlinerStore, {
   root: observable,
   options: observable,
   fetchOutliner: action.bound,
-  toggleColVisible: action.bound
+  toggleColVisible: action.bound,
+  toggleRowCollapsed: action.bound,
 })
 
 export default OutlinerStore;
