f4b4305ca1dd38b5fbaf48a1014380478d706e57
Fang <windmaomao@gmail.com>
2019-11-10 15:49:54 -0500

    add basic brand

--- brand/src/index.js
+++ brand/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Book } from 'react-brandbook'
+
+const themes = [
+  { title: 'Light', theme: { mode: 'light' } },
+  { title: 'Dark', theme: { mode: 'dark' } }
+]
+const layouts = [
+  { title: 'Top', direction: 'column' },
+  { title: 'Left', direction: 'row' }
+]
+const topics = [{ title: 'Home', stories: [] }]
+const title = 'QPLOT'
+const logo = <img alt="" src={'./logo.png'} width={'24px'} />
 
 ReactDOM.render(
-  <div>Hello World</div>, 
+  <Book
+    title={title}
+    logo={logo}
+    topics={topics}
+    themes={themes}
+    layouts={layouts}
+  />, 
   document.getElementById('root')
 )
