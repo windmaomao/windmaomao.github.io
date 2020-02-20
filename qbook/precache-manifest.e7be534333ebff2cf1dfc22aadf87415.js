9f6d0419e0199183225e0b5a8af38e93cb1a4ef6
Fang <windmaomao@gmail.com>
2020-02-19 21:02:07 -0500

    redeploy qbook

---
+++ qbook/precache-manifest.e7be534333ebff2cf1dfc22aadf87415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3de8248eaeae7b19ab3ff3f660b5798c",
+    "url": "./static/media/logo.3de8248e.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "623b326b568ed947d971",
+    "url": "./static/js/main.623b326b.chunk.js"
+  },
+  {
+    "revision": "b6068ac7f424659e7410",
+    "url": "./static/js/2.b6068ac7.chunk.js"
+  },
+  {
+    "revision": "6cdd970634886bd81eefd12d335e65e4",
+    "url": "./index.html"
+  }
+];
--- qbook/service-worker.js
+++ qbook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f05856c5b859c023a8140365dcef1e3.js"
+  "./precache-manifest.e7be534333ebff2cf1dfc22aadf87415.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
